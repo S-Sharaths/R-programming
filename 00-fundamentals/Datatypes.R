a4676ed425a7a55d7edf3535ad7c5ef39cfc8cd6
S-Sharaths <sharathprogramming4@gmail.com>
2024-05-06 03:39:55 +0530

    first commit

---
+++ 00-fundamentals/Datatypes.R
@@ -0,0 +1,23 @@
+
+#integer
+   x <- 2L
+   typeof(x)
+   
+#double 
+   y<-2.5;
+   typeof(y);
+   
+#without L it will R will aoutmatically store it as double 
+   
+   x <- 2
+   typeof(x)
+   
+#complex number
+   
+   z <- 3+2i
+   typeof(z)
+
+#character 
+   
+   a <- "h"
+   typeof(a)
